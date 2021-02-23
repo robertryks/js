1659480a7eb9970d42aa6d82238a1ed6707f9211
Robert Ryks <rykrob@gmail.com>
2021-02-23 12:12:36 +0100

    nowy index.html struktura pod todo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-function add(a,b) {
-    return a+b;
-}
+const btn1 = document.querySelector(".one");
+const btn2 = document.querySelector(".two");
+const span1 = document.querySelector(".original");
+const span2 = document.querySelector(".new");
 
-console.log(add(15,2));
+let $score;
+
+const add = (a, b) => {
+  $score = a + b;
+  span1.innerHTML = $score;
+};
+
+const multiply = () => {
+  span2.innerHTML = $score * 2;
+};
+const divide = () => {
+  span2.innerHTML = $score / 2;
+};
+
+btn1.addEventListener("click", multiply);
+btn2.addEventListener("click", divide);
+
+add(8, 13);
+
+if ($score >= 0) {
+  console.log("Odpalone Fira Code");
+}
